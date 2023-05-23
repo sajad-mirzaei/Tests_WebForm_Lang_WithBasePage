0ec0ca9562969c3eff0553372edeb2afcdf825b4
mirzaei.sajad@hotmail.com <mirzaei.sajad@hotmail.com>
2023-05-23 16:31:35 +0330

    Added GetLocalResourceObject in static method

--- src/Lang/Default.aspx.cs
+++ src/Lang/Default.aspx.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Globalization;
 using System.Security.Policy;
+using System.Web;
 using System.Web.UI;
 
 public partial class _Default : mybasePage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        var x = GetLocalResourceObject("TestLabel1");
-        if(!IsPostBack)
+        //روش های مختلف فراخوانی
+        var getLocalResourceObject_In_Static_method = StaticMethod(Page.AppRelativeVirtualPath);
+        var GetLocalResourceObject1 = GetLocalResourceObject("TestLabel1").ToString();
+        var GetLocalResourceObject2 = ResourceHelper.GetLocalResource("TestLabel1");
+        var GetLocalResourceObject3 = HttpContext.GetLocalResourceObject(Page.AppRelativeVirtualPath, "TestLabel1").ToString();
+        //var GetLocalResourceObject3 = HttpContext.GetLocalResourceObject("~/Default.aspx", "TestLabel1").ToString(); // برای زمانی که آدرس فایل را داریم
+        if (!IsPostBack)
             DropDownList1.SelectedValue = CultureInfo.CurrentCulture.Name;
     }
     protected void Button1_Click(object sender, EventArgs e)
@@ -16,5 +22,18 @@ public partial class _Default : mybasePage
         var c = DropDownList1.SelectedItem.Value.ToString();
         mybasePage.CultureName = c;
         Page.Response.Redirect(Request.Url.OriginalString);
+    }
+    public static string StaticMethod(string appRelativeVirtualPath)
+    {
+        return HttpContext.GetLocalResourceObject(appRelativeVirtualPath, "TestLabel1").ToString();
+        //return HttpContext.GetLocalResourceObject("~/Default.aspx", "TestLabel1").ToString(); // برای زمانی که آدرس فایل را داریم
+    }
+}
+
+public static class ResourceHelper
+{
+    public static string GetLocalResource(string resourceKey)
+    {
+        return HttpContext.GetLocalResourceObject("~/Default.aspx", resourceKey).ToString();
     }
 }
